41d2e41b1de64a0d57f05f1112859a602b93ce61
Mauricio Cuello <mauricio.cuello@gmail.com>
2024-10-15 20:19:31 +0200

    Bash script to use as user data

--- basic_api/SQS_MySQL/install.sh
+++ basic_api/SQS_MySQL/install.sh
@@ -8,8 +8,12 @@ apt install git curl python3.12 python3-pip python3-venv
 mkdir prod
 cd prod
 
-git clone 
+git clone https://github.com/MauricioD13/Intro_to_terraform/tree/main/basic_api/SQS_MySQL/app
 python3 -m venv .env
 
 source .env/bin/activate
+
+pip3 install -r requirements.txt
+
+uvicorn 
 
