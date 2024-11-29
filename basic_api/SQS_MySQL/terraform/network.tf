0bef78616821e05311a0363dd881c35f89b4b8d2
Mauricio Cuello <mauricio.cuello@gmail.com>
2024-11-29 10:03:25 +0100

    Subnet and SG

--- basic_api/SQS_MySQL/terraform/network.tf
+++ basic_api/SQS_MySQL/terraform/network.tf
@@ -15,37 +15,6 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "private" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = false
-}
-
-# Internet Gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_eip" "main" {
-  depends_on = [aws_internet_gateway.igw]
-}
-
-# Route Table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-# Route Table Association
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
 # EC2 Security Group
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2_sg"
