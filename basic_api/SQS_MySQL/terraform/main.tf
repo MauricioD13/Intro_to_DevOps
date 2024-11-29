abf22cf0cbcac53ca13bd1044363d6f91bb76fbf
Mauricio Cuello <mauricio.cuello@gmail.com>
2024-11-29 10:04:44 +0100

    EC2 instances config

--- basic_api/SQS_MySQL/terraform/main.tf
+++ basic_api/SQS_MySQL/terraform/main.tf
@@ -1,23 +1,29 @@
-resource "aws_sqs_queue" "queue" {
-  name = "my-queue"
-}
-
 data "aws_ami" "main" {
   most_recent = true
-  owners = [ "amazon" ]
+  owners      = ["amazon"]
   filter {
-    name = "architecture"
+    name   = "architecture"
     values = ["arm64"]
   }
   filter {
-    name = "name"
-    values = [ "al2023-ami-2023" ]
+    name   = "name"
+    values = ["al2023-ami-2023"]
   }
 
 }
 
 resource "aws_instance" "main" {
-  ami = data.aws_ami.main.id
-  instance_type = "t2.micro"
-  
+  ami                         = data.aws_ami.main.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 20
+  }
+  key_name = "recipe_key"
+  tags = {
+    Name = "RecipeApp"
+  }
 }
