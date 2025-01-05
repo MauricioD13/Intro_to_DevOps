29139b012d791cc58c7f5494d6d9524fb485ebb1
Mauricio Cuello <mauricio.cuello@gmail.com>
2025-01-05 22:34:31 +0100

    Bash script for deployment

---
+++ basic_api/SQS_MySQL/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+echo "1. Initializing Terraform..."
+
+cd terraform
+terraform init
+
+echo "2. Applying Terraform configuration..."
+terraform apply -auto-approve -var="key_name=tf_key"
+
+echo "3. Getting EC2 instance IP..."
+INSTANCE_IP=$(terraform output -raw instance_public_ip)
+
+echo "4. Waiting for instance to be ready..."
+sleep 60
+
+echo "5. Creating Ansible inventory..."
+cd ../ansible
+sed "s/\${terraform_ip}/$INSTANCE_IP/" inventory.ini.template > inventory.ini
+
+echo "6. Running Ansible playbook..."
+ansible-playbook -i inventory.ini setup.yml
+
+echo "Done! Your application is now deployed at http://$INSTANCE_IP:8000"
