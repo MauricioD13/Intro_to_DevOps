5ba2abbe5637597593911f158907bf529da9e1e5
Mauricio Cuello <mauricio.cuello@gmail.com>
2025-01-06 22:04:06 +0100

    Rename

---
+++ basic_api/FastAPI_MySQL/install_tools.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# Install Ansible
+sudo apt install -y unzip pipx python3-pip python3 gnupg software-properties-common curl
+python3 -m pipx ensurepath
+source ~/.bashrc
+pipx install --include-deps ansible
+
+# Install Terraform
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install -y terraform
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Verify installation
+terraform --version
+ansible --version
+aws --version
