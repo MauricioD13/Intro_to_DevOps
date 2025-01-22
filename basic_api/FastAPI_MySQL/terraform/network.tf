3e207b8ea3a132dc1afb8b4ffe240353153f64b2
Mauricio Cuello <mauricio.cuello@gmail.com>
2025-01-22 22:51:38 +0100

    IGW

--- basic_api/FastAPI_MySQL/terraform/network.tf
+++ basic_api/FastAPI_MySQL/terraform/network.tf
@@ -7,6 +7,29 @@ resource "aws_vpc" "main" {
     Name = "main-vpc"
   }
 }
+# INTERNET GATEWAY
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+# ROUTE TABLE
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "public-route-table"
+  }
+}
+# ROUTE TABLE ASSOCIATION
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
 
 # SUBNETS
 resource "aws_subnet" "public" {
@@ -14,6 +37,9 @@ resource "aws_subnet" "public" {
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   depends_on              = [aws_vpc.main]
+  tags = {
+    Name = "public-subnet"
+  }
 }
 
 # EC2 Security Group
