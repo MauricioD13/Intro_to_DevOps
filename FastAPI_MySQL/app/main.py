4c8dd436629dd379eaa016b5916ba15cd16bf4d5
Mauricio Cuello <mauricio.cuello@gmail.com>
2025-01-26 17:50:48 +0100

    fix: change in TemplateResponse method

--- FastAPI_MySQL/app/main.py
+++ FastAPI_MySQL/app/main.py
@@ -33,7 +33,7 @@ templates = Jinja2Templates(directory="templates")
 # SIGN UP
 @app.get("/signup", response_class=HTMLResponse)
 async def get_users(request: Request):
-    return templates.TemplateResponse("signup.html", {"request": request})
+    return templates.TemplateResponse(request,name="signup.html")
 
 
 # Endpoint to handle user sign-up
@@ -70,9 +70,8 @@ def read_user(request: Request, user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    print(db_user)
     return templates.TemplateResponse(
-        "user_id.html", {"request": request, "db_user": db_user}
+         request ,name="user_id.html",context ={"db_user": db_user}
     )
 
 
@@ -82,7 +81,7 @@ def get_users(
 ):
     users = crud.get_users(db, skip=skip, limit=limit)
     return templates.TemplateResponse(
-        "users.html", {"request": request, "users": users}
+        request,name="users.html", context={ "users": users}
     )
 
 
@@ -91,7 +90,7 @@ def get_users(
 
 @app.get("/", response_class=HTMLResponse)
 def home_page(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse(request,name="home.html")
 
 
 # RECIPES
@@ -105,5 +104,4 @@ def read_recipes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
 def create_recipe_for_user(
     user_id: int, recipe: schemas.RecipeCreate, db: Session = Depends(get_db)
 ):
-    print(recipe.ingredients)
     return crud.create_user_recipe(db=db, recipe=recipe, user_id=int(user_id))
