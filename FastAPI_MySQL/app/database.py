d55d45f9f8993b356fa0512c49ef103b9f9de7c3
Mauricio Cuello <mauricio.cuello@gmail.com>
2025-02-14 23:16:11 +0100

    Fix to discard issues

--- FastAPI_MySQL/app/database.py
+++ FastAPI_MySQL/app/database.py
@@ -19,10 +19,10 @@ with open('data.json', 'r') as file:
 
 SQLALCHEMY_DATABASE_URL = URL.create(
     "mysql+pymysql",
-    username = os.environ.get("DB_USER"),
-    password = os.environ.get("DB_PASS"),
-    host = os.environ.get("DB_HOST"),
-    database = os.environ.get("DB_NAME"),
+    username = "fastapi",
+    password = "fastapi",
+    host = "localhost",
+    database = "recipes",
 )
 engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"check_same_thread": False})
 
